a6d0538a4add8a118d3a887cfa131ef9636302c3
Justin Williams <justinwdev@gmail.com>
2022-01-06 15:01:47 -0500

    created auth and impersonate view

---
+++ resources/views/impersonate.blade.php
@@ -0,0 +1,38 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Impersonate') }}
+        </h2>
+    </x-slot>
+    <x-auth-card>
+
+
+        <!-- Session Status -->
+        <x-auth-session-status class="mb-4" :status="session('status')" />
+
+        <!-- Validation Errors -->
+        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+
+            <!-- Email Address -->
+            <div>
+                <x-label for="email" :value="__('Email')" />
+
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            </div>
+
+
+
+
+            <div class="flex items-center justify-end mt-4">
+
+
+                <x-button class="ml-3">
+                    {{ __('Impersonate') }}
+                </x-button>
+            </div>
+        </form>
+    </x-auth-card>
+</x-app-layout>
--- tests/Feature/ImpersonateTest.php
+++ tests/Feature/ImpersonateTest.php
@@ -2,21 +2,33 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ImpersonateTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_non_admin_can_not_access()
     {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+
+        $this->get('/admin/impersonate')->assertRedirect('/login');
+
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $this->get('/admin/impersonate')->assertStatus(403);
+
+
+
+
+
     }
 }
